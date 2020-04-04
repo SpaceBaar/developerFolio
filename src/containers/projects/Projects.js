8e1d7af9ec56cc6bb6855cf8864bc40a0c66daaf
Ashitosh Yadav <ashitoshy@moofwd.com>
2020-04-04 21:01:51 +0530

    formatting

--- src/containers/projects/Projects.js
+++ src/containers/projects/Projects.js
@@ -8,27 +8,27 @@ import { openSource } from "../../portfolio";
 import { Fade } from "react-reveal";
 
 export default function Projects() {
-  const [repo, setrepo] = useState([]);
+    const [repo, setrepo] = useState([]);
 
-  useEffect(() => {
-    getRepoData();
-  }, []);
+    useEffect(() => {
+        getRepoData();
+    }, []);
 
-  function getRepoData() {
-    const client = new ApolloClient({
-      uri: "https://api.github.com/graphql",
-      request: operation => {
-        operation.setContext({
-          headers: {
-            authorization: `Bearer ${atob(openSource.githubConvertedToken)}`
-          }
+    function getRepoData() {
+        const client = new ApolloClient({
+            uri: "https://api.github.com/graphql",
+            request: operation => {
+                operation.setContext({
+                    headers: {
+                        authorization: `Bearer ${atob(openSource.githubConvertedToken)}`
+                    }
+                });
+            }
         });
-      }
-    });
 
-    client
-      .query({
-        query: gql`
+        client
+            .query({
+                query: gql`
           {
             repositoryOwner(login: "${openSource.githubUserName}") {
               ... on User {
@@ -55,28 +55,28 @@ export default function Projects() {
             }
           }
         `
-      })
-      .then(result => {
-        setrepoFunction(result.data.repositoryOwner.pinnedRepositories.edges);
-        console.log(result);
-      });
-  }
+            })
+            .then(result => {
+                setrepoFunction(result.data.repositoryOwner.pinnedRepositories.edges);
+                console.log(result);
+            });
+    }
 
-  function setrepoFunction(array) {
-    setrepo(array);
-  }
+    function setrepoFunction(array) {
+        setrepo(array);
+    }
 
-  return (
-    <Fade bottom duration={1000} distance="20px">
-    <div className="main" id="opensource">
-      <h1 className="project-title">Open Source Projects</h1>
-      <div className="repo-cards-div-main">
-        {repo.map((v, i) => {
-          return <GithubRepoCard repo={v} key={v.node.id} />;
-        })}
-      </div>
-      <Button text={"More Projects"} className="project-button" href="https://github.com/saadpasta" newTab={true} />
-    </div>
-    </Fade>
-  );
+    return (
+        <Fade bottom duration={1000} distance="20px">
+            <div className="main" id="opensource">
+                <h1 className="project-title">Open Source Projects</h1>
+                <div className="repo-cards-div-main">
+                    {repo.map((v, i) => {
+                        return <GithubRepoCard repo={v} key={v.node.id} />;
+                    })}
+                </div>
+                <Button text={"More Projects"} className="project-button" href="https://github.com/SpaceBaar" newTab={true} />
+            </div>
+        </Fade>
+    );
 }
